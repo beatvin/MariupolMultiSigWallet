c1c9a9b3ea5ffce0a2cf94a8b242824287dcd406
beatvin <beatvin128@gmail.com>
2022-09-16 01:31:04 +0300

    Changed IERC20

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.17;
 
 interface IERC20 {
-    
+
     function balanceOf(address _address) external view returns (uint256);
 
     function transfer(address to, uint256 amount) external returns (bool);
